b6147e2644b2a87e8b6ec3437eefb19efb3e99e8
tanvir <tanvirulislam149@gmail.com>
2026-03-27 20:32:08 +0600

    49 done

--- Array/49. Group Anagrams.cpp
+++ Array/49. Group Anagrams.cpp
@@ -5,31 +5,22 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> v;
-        vector<string> temp;
-        for(int i = 0; i < strs.size(); i++){
-            string str1 = strs[i];
-            sort(str1.begin(), str1.end());
-            if(find(temp.begin(), temp.end(), str1) != temp.end()){
-                continue;
+        map<string, vector<string>> str_map;
+        for(string str : strs){
+            string s = str;
+            sort(s.begin(), s.end());
+            if(str_map.find(s) == str_map.end()){  // didn't find the key in map
+                str_map.insert({s, {str}});
+            } else {
+                str_map[s].push_back(str);
             }
-
-            temp.push_back(str1);
-            
-            vector<string> s1;
-            for(int j = i+1; j < strs.size()-1; j++){
-                string str2 = strs[j];
-                sort(str2.begin(), str2.end());
-                if(str1 == str2){
-                    if(s1.empty()){
-                        s1.push_back(strs[i]);
-                    }
-                    s1.push_back(strs[j]);
-                }
+        }
+        for(auto it : str_map){
+            vector<string> temp_v;
+            for(string s : it.second){
+                temp_v.push_back(s);
             }
-            if(i == strs.size()-1){
-                s1.push_back({strs[i]});
-            }
-            v.push_back(s1);
+            v.push_back(temp_v);
         }
         return v;
     }
